c140cd1adb9cece10cef909cea9df3766cb6012f
PhantomOz <faniogor@gmail.com>
2025-06-06 13:34:59 +0100

    This adds ccip receive facet

--- contracts/facets/CcipFacet.sol
+++ contracts/facets/CcipFacet.sol
@@ -1,6 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
+import {CCIPReceiver} from "../utils/functions/CCIPReceiver.sol";
+import {AppStorage} from "../utils/functions/AppStorage.sol";
+import {CCIPMessageReceived, CCIPMessageExecuted} from "../model/Event.sol";
+import {CCIPMessageType} from "../model/Protocol.sol";
+import {LibCCIP} from "../libraries/LibCCIP.sol";
+import {LibAppStorage} from "../libraries/LibAppStorage.sol";
 
-contract CcipFacet is AppStorage, CCIPReceiver, CCIPSender {}
+/**
+ * @title CcipFacet
+ * @notice This facet is responsible for handling CCIP messages.
+ */
+contract CcipFacet is AppStorage, CCIPReceiver {
+    using LibCCIP for LibAppStorage.Layout;
+
+    /**
+     * @dev This function is called when a CCIP message is received.
+     * @param message The CCIP message received.
+     * message contains:
+     * - messageId: The ID of the message.
+     * - sourceChainSelector: The chain selector of the source chain.
+     * - sender: The sender of the message.
+     * - data: The data of the message.
+     * - destTokenAmounts: The token amounts in the destination chain representation.
+     */
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        emit CCIPMessageReceived(
+            message.messageId,
+            message.sourceChainSelector,
+            message.sender,
+            message.destTokenAmounts
+        );
+
+        //decode the data
+        (CCIPMessageType messageType, bytes memory messageData) = abi.decode(
+            message.data,
+            (CCIPMessageType, bytes)
+        );
+
+        _appStorage._resolveCCIPMessage(
+            messageType,
+            messageData,
+            message.sourceChainSelector
+        );
+
+        emit CCIPMessageExecuted(
+            message.messageId,
+            message.sourceChainSelector,
+            message.sender,
+            message.destTokenAmounts
+        );
+    }
+}
--- contracts/model/Event.sol
+++ contracts/model/Event.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
+import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
+
 event RequestCreated(
-    address indexed _borrower, uint96 indexed requestId, uint256 _amount, uint16 _interest, uint64 _chainSelector
+    address indexed _borrower,
+    uint96 indexed requestId,
+    uint256 _amount,
+    uint16 _interest,
+    uint64 _chainSelector
 );
 
-event OrderCreated(address indexed _lender, address indexed_tokenAddress, uint256 _amount, uint96 indexed _requestId);
+event OrderCreated(
+    address indexed _lender,
+    address indexed_tokenAddress,
+    uint256 _amount,
+    uint96 indexed _requestId
+);
 
-event RespondToLendingOffer(address indexed sender, uint256 indexed _offerId, uint8 _status, uint8 _offerStatus);
+event RespondToLendingOffer(
+    address indexed sender,
+    uint256 indexed _offerId,
+    uint8 _status,
+    uint8 _offerStatus
+);
 
 event RequestServiced(
     uint96 indexed _requestId,
@@ -17,31 +33,69 @@ event RequestServiced(
     uint64 _chainSelector
 );
 
-event RequestClosed(uint96 indexed _requestId, address indexed _borrower, uint64 _chainSelector);
+event RequestClosed(
+    uint96 indexed _requestId,
+    address indexed _borrower,
+    uint64 _chainSelector
+);
 
 event CollateralWithdrawn(
-    address indexed sender, address indexed _tokenCollateralAddress, uint256 _amount, uint64 _chainSelector
+    address indexed sender,
+    address indexed _tokenCollateralAddress,
+    uint256 _amount,
+    uint64 _chainSelector
 );
 
 event UpdatedCollateralTokens(address indexed sender, uint8 newTokensCount);
 
-event AcceptedListedAds(address indexed sender, uint96 indexed id, uint256 indexed amount, uint8 adStatus);
+event AcceptedListedAds(
+    address indexed sender,
+    uint96 indexed id,
+    uint256 indexed amount,
+    uint8 adStatus
+);
 
-event LoanRepayment(address indexed sender, uint96 id, uint256 amount, uint64 chainSelector);
+event LoanRepayment(
+    address indexed sender,
+    uint96 id,
+    uint256 amount,
+    uint64 chainSelector
+);
 
-event UpdateLoanableToken(address indexed _token, address _priceFeed, address indexed sender);
+event UpdateLoanableToken(
+    address indexed _token,
+    address _priceFeed,
+    address indexed sender
+);
 
-event CollateralDeposited(address indexed _sender, address indexed _token, uint256 _value, uint64 _chainSelector);
+event CollateralDeposited(
+    address indexed _sender,
+    address indexed _token,
+    uint256 _value,
+    uint64 _chainSelector
+);
 
 event withdrawnAdsToken(
-    address indexed sender, uint96 indexed _orderId, uint8 indexed orderStatus, uint256 _amount, uint64 _chainSelector
+    address indexed sender,
+    uint96 indexed _orderId,
+    uint8 indexed orderStatus,
+    uint256 _amount,
+    uint64 _chainSelector
 );
 
 event LoanListingCreated(
-    uint96 indexed listingId, address indexed sender, address indexed tokenAddress, uint256 amount, uint64 chainSelector
+    uint96 indexed listingId,
+    address indexed sender,
+    address indexed tokenAddress,
+    uint256 amount,
+    uint64 chainSelector
 );
 
-event RequestLiquidated(uint96 indexed requestId, address indexed lenderAddress, uint256 indexed totalRepayment);
+event RequestLiquidated(
+    uint96 indexed requestId,
+    address indexed lenderAddress,
+    uint256 indexed totalRepayment
+);
 
 event FeesWithdrawn(address indexed to, address indexed token, uint256 amount);
 
@@ -69,7 +123,12 @@ event ProtocolPoolInitialized(address indexed token, uint256 reserveFactor);
  * @param amount The amount of tokens deposited
  * @param sharesMinted The amount of LP shares minted to the user
  */
-event Deposit(address indexed user, address indexed token, uint256 amount, uint256 sharesMinted);
+event Deposit(
+    address indexed user,
+    address indexed token,
+    uint256 amount,
+    uint256 sharesMinted
+);
 
 /**
  * @notice Emitted when a user withdraws from a liquidity pool
@@ -78,7 +137,12 @@ event Deposit(address indexed user, address indexed token, uint256 amount, uint2
  * @param amount The amount of tokens withdrawn
  * @param sharesBurned The amount of LP shares burned
  */
-event Withdraw(address indexed user, address indexed token, uint256 amount, uint256 sharesBurned);
+event Withdraw(
+    address indexed user,
+    address indexed token,
+    uint256 amount,
+    uint256 sharesBurned
+);
 
 /**
  * @notice Emitted when interest is accrued in a pool
@@ -87,7 +151,45 @@ event Withdraw(address indexed user, address indexed token, uint256 amount, uint
  */
 event InterestAccrued(address indexed token, uint256 interestAccrued);
 
-event Borrow(address indexed sender, address indexed token, uint256 indexed amount);
+event Borrow(
+    address indexed sender,
+    address indexed token,
+    uint256 indexed amount
+);
 
-event Repay(address indexed sender, address indexed token, uint256 indexed amountRepaid);
+event Repay(
+    address indexed sender,
+    address indexed token,
+    uint256 indexed amountRepaid
+);
 // event Withdraw(address indexed sender,address indexed token,uint256 amountWithdrawn,uint256 shares);
+
+// CCIP Events
+
+event CCIPMessageReceived(
+    bytes32 indexed messageId,
+    uint64 indexed sourceChainSelector,
+    bytes indexed sender,
+    Client.EVMTokenAmount[] destTokenAmounts
+);
+
+event CCIPMessageSent(
+    bytes32 indexed messageId,
+    uint64 indexed sourceChainSelector,
+    bytes indexed sender,
+    Client.EVMTokenAmount[] destTokenAmounts
+);
+
+event CCIPMessageFailed(
+    bytes32 indexed messageId,
+    uint64 indexed sourceChainSelector,
+    bytes indexed sender,
+    Client.EVMTokenAmount[] destTokenAmounts
+);
+
+event CCIPMessageExecuted(
+    bytes32 indexed messageId,
+    uint64 indexed sourceChainSelector,
+    bytes indexed sender,
+    Client.EVMTokenAmount[] destTokenAmounts
+);
